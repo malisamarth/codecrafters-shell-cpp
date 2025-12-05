9660440dec4a5bade94b101bf9ce13d34e7b83b9
Samarth Kautik Mali <141520621+malisamarth@users.noreply.github.com>
2025-12-06 00:45:33 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,30 @@
 #include <string>
 
 int main() {
+  
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+  bool programIsRunning = true;
+  
+  while (programIsRunning) {
+    
+  
+    std::string input;
+    std::getline(std::cin, input);
+  
+    if(input == "exit") {
+      programIsRunning = false;
+    } else {
+      std::cout << input << ": command not found" << std::endl;
+    
+      main();
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+    }
 
-  main();
+  }
+  
 }
