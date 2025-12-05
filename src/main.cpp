d69fab06b2afa95eff9047848f19f7aa747a4a93
Samarth Kautik Mali <141520621+malisamarth@users.noreply.github.com>
2025-12-06 02:23:01 +0530

    added type checking for echo, exit and type

--- src/main.cpp
+++ src/main.cpp
@@ -2,65 +2,73 @@
 #include <string>
 using namespace std;
 int main() {
-  // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
+    // Flush after every std::cout / std:cerr
+    cout << unitbuf;
+    cerr << unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  while(true){
+    // TODO: Uncomment the code below to pass the first stage
+    while (true) {
 
-    cout << "$ ";
-    string command;
-    getline(cin, command);
+        cout << "$ ";
+        string command;
+        getline(cin, command);
 
-    if(command == "exit") {
-      break;
-    } else {
+        if (command == "exit") {
+            break;
+        }
+        else {
 
-      int currentIndex = 0;
-      int commandLength = command.length();
-      string inputCommand = "";
-      bool inputCommandParsed = false;
-      string userInputString = "";
+            int currentIndex = 0;
+            int commandLength = command.length();
+            string inputCommand = "";
+            bool inputCommandParsed = false;
+            string userInputString = "";
 
-      while (commandLength >= currentIndex) {
-        
-        if (command[currentIndex] != ' ' && !inputCommandParsed) {
-          inputCommand += command[currentIndex];
-          currentIndex++;
-        } else {
-          inputCommandParsed = true;
-          currentIndex++;
+            while (currentIndex < commandLength) {
 
-          if (currentIndex <= commandLength) {
-            userInputString += command[currentIndex];
-          }
+                if (command[currentIndex] != ' ' && !inputCommandParsed) {
+                    inputCommand += command[currentIndex];
+                    currentIndex++;
+                }
+                else {
+                    inputCommandParsed = true;
+
+                    if (currentIndex < commandLength) {
+                        userInputString += command[currentIndex];
+                    }
+                    currentIndex++;
+                }
+
+            }
+
+            if (!userInputString.empty() && userInputString[0] == ' ') {
+                userInputString.erase(0, 1);
+            }
+
+            if (inputCommand == "type") {
+
+                if (userInputString == "echo" || userInputString == "exit" || userInputString == "type") {
+                    std::cout << userInputString << " is a shell builtin" << std::endl;
+                }
+                else {
+                    std::cout << userInputString << " : not found" << std::endl;
+                }
+
+            }
+            else {
+                if (inputCommand == "echo") {
+                    cout << userInputString << endl;
+                }
+                else {
+                    cout << command << ": command not found" << endl;
+                }
+
+            }
+
 
         }
 
-      }
-
-      if (inputCommand == "type") {
-
-        if (userInputString == "echo" || userInputString == "exit" || userInputString == "type") {
-          std::cout << userInputString << " is a shell builtin" << std::endl; 
-        } else {
-          std::cout << userInputString << " : not found" << std::endl; 
-        }
-
-      } else {
-        if (inputCommand == "echo") {
-          cout << userInputString << endl ;  
-        } else {
-          cout << command << ": command not found"<<endl ;
-        }
-
-      }
-
-      
     }
-      
-  }
 
 
 }
